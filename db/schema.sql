67d48b3e6a30cd7138459e4dfac44ca3a764bbca
Joe Fogg <GJF610@gmail.com>
2017-11-01 20:37:22 -0400

    Updated burger_queen.

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+	id INTEGER,
+	name VARCHAR(30)NOT NULL,
+    devoured BOOLEAN,
+    date TIMESTAMP
+    );
+    
+
+
+    
+ 
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE `burgers_db`;
+
+-- insert 3 burgers
+	INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Wheres The Beef', false, CURRENT_TIMESTAMP);
+	INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Cheezeburger Cheezeburgerf', false, CURRENT_TIMESTAMP);
+	INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Big Mamas Wonder Bread Burger', false, CURRENT_TIMESTAMP);
